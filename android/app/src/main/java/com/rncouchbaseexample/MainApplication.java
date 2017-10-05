0fb6ff299e4161c9a83854609116b395f3f48927
Dyego W. Santin <dyegows@gmail.com>
2017-10-05 17:07:07 -0300

    Removing the database creatio1on native Java.

--- android/app/src/main/java/com/rncouchbaseexample/MainApplication.java
+++ android/app/src/main/java/com/rncouchbaseexample/MainApplication.java
@@ -1,20 +1,13 @@
 package com.rncouchbaseexample;
 
 import android.app.Application;
-import android.util.Log;
 
-import com.couchbase.lite.CouchbaseLiteException;
 import com.couchbase.lite.Database;
-import com.couchbase.lite.DatabaseOptions;
-import com.couchbase.lite.Manager;
-import com.couchbase.lite.android.AndroidContext;
 import com.facebook.react.ReactApplication;
-import com.facebook.react.ReactInstanceManager;
 import com.facebook.react.ReactNativeHost;
 import com.facebook.react.ReactPackage;
 import com.facebook.react.shell.MainReactPackage;
 
-import java.io.IOException;
 import java.util.Arrays;
 import java.util.List;
 
@@ -46,52 +39,4 @@ public class MainApplication extends Application implements ReactApplication {
       return mReactNativeHost;
   }
 
-  @Override
-  public void onCreate() {
-    super.onCreate();
-
-    //enableLogging();
-    //openDatabase("password123456");
-    //openDatabase(null);
-    //closeDatabase();
-  }
-
-
-
-  private void openDatabase(String key) {
-    DatabaseOptions options = new DatabaseOptions();
-    options.setCreate(true);
-
-    if (encryptionEnabled) {
-      options.setEncryptionKey(key);
-    }
-
-    Manager manager = null;
-    try {
-      manager = new Manager(new AndroidContext(getApplicationContext()), Manager.DEFAULT_OPTIONS);
-    } catch (IOException e) {
-      e.printStackTrace();
-    }
-    try {
-      database = manager.openDatabase("smt", options);
-      //manager.registerEncryptionKey(null, "smt");
-      database.changeEncryptionKey(null);
-      //database.close();
-
-    } catch (CouchbaseLiteException e) {
-      e.printStackTrace();
-    }
-  }
-
-
-
-  private void enableLogging() {
-    Manager.enableLogging("smt", Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG, Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG_SYNC_ASYNC_TASK, Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG_SYNC, Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG_QUERY, Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG_VIEW, Log.VERBOSE);
-    Manager.enableLogging(com.couchbase.lite.util.Log.TAG_DATABASE, Log.VERBOSE);
-  }
 }
